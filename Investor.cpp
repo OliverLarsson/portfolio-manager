40d521a4cb2a0f8d1490fa7a7cb16f9612b24016
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-13 13:49:19 -0600

    Investor structure

---
+++ Investor.cpp
@@ -0,0 +1,14 @@
+/**
+ * Investor.cpp 
+ * Deals with all information related to the investor 
+*/ 
+
+// Header file
+#include "Investor.h" 
+
+// Libraries 
+#include <iostream> 
+
+using namespace std; 
+
+// Methods 
---
+++ Investor.h
@@ -0,0 +1,23 @@
+/**
+ * Investor.h
+ * Header for the Investor class
+*/ 
+
+#ifndef _INVESTOR_H_
+#define _INVESTOR_H_
+
+// Libraries
+#include <iostream> 
+
+using namespace std; 
+
+class Investor {
+    public: 
+    // public methods go here 
+
+    private: 
+    //private methods go here 
+
+} // class Investor
+
+#endif //_INVESTOR_H_ 
