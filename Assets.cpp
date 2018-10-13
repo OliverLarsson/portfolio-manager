1a3f1881901d1d510f64ff2fd8556f1bf4e7318b
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-13 13:54:51 -0600

    Assets structure

---
+++ Assets.cpp
@@ -0,0 +1,14 @@
+/**
+ * Assets.cpp 
+ * Deals with all information related to the financial assets 
+*/ 
+
+// Header file
+#include "Assets.h" 
+
+// Libraries 
+#include <iostream> 
+
+using namespace std; 
+
+// Methods 
---
+++ Assets.h
@@ -0,0 +1,23 @@
+/**
+ * Assets.h
+ * Header for the Assets class
+*/ 
+
+#ifndef _ASSETS_H_
+#define _ASSETS_H_
+
+// Libraries
+#include <iostream> 
+
+using namespace std; 
+
+class Assets {
+    public: 
+    // public methods go here 
+
+    private: 
+    //private methods go here 
+
+}; // class Assets
+
+#endif //_ASSETS_H_ 
