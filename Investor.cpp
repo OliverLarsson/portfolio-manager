5510fc126991e75f0ca27d35acf403c9a1264cb9
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-13 17:53:09 -0600

    Additional risk parameters

--- Investor.cpp
+++ Investor.cpp
@@ -17,11 +17,12 @@ using namespace std;
  * Constructor
  * Takes four parameters from user input: age, wealth, risk tolerance, and sector preference
 */ 
-Investor::Investor(string name, int age, int wealth, int risk, Sector sector) {
+Investor::Investor(string name, int age, int wealth, int risk_tolerance, int risk_required, Sector sector) {
     name_ = name; 
     age_ = age; 
     wealth_ = wealth; 
-    risk_ = risk; 
+    risk_tolerance_ = risk_tolerance; 
+    risk_required_ = risk_required; 
     sector_ = sector; 
 }
 
@@ -38,4 +39,14 @@ string Investor::get_sector() { // might use a map here if it's necessary / more
         sec = "Industrial"; 
     }
     return sec; 
+}
+
+/**
+ * risk_profile
+ * Takes age, wealth, and three risk attributes from the Investor object
+ * Creates a risk profile based on _____
+ * Returns a double from 0-1, 0 being the lowest risk tolerance 
+*/ 
+double Investor::risk_profile() {
+    return get_risk_capacity() + risk_required_ + risk_tolerance_; 
 }
--- Investor.h
+++ Investor.h
@@ -15,21 +15,26 @@ enum class Sector { tech, industrial };
 
 class Investor {
     public: 
-    Investor(string name, int age, int wealth, int risk, Sector sector); // constructor
+    Investor(string name, int age, int wealth, int risk_tolerance, int risk_required, Sector sector); // constructor
 
-    string get_name() { return name_; }; // inline implementation 
-    int get_age() { return age_; }; // inline 
+    string get_name() { return name_; }; // inline implementation
+    int get_age() { return age_; }; // inline
     int get_wealth() { return wealth_; }; //inline
-    int get_risk() { return risk_; }; //inline
+    int get_risk_tolerance() { return risk_tolerance_; }; //inline
+    int get_risk_required() { return risk_required_; }; //inline
+    int get_risk_capacity() {
+        return 2; 
+    };
     string get_sector(); 
+    double risk_profile(); 
 
     private: 
     string name_; 
     int age_; 
     int wealth_; 
-    int risk_; // what the investor claims
+    int risk_tolerance_; // what the investor claims
+    int risk_required_;
     Sector sector_; 
-    //int risk_profile; // what is calculated by Investor::risk_profile; 
 
 }; // class Investor
 
