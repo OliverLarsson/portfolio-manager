d8f65aa5c5f8f8ccb810e7c9eb9e61a72b1fb7af
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-13 13:58:30 -0600

    Portfolio structure

---
+++ Portfolio.cpp
@@ -0,0 +1,14 @@
+/**
+ * Portfolio.cpp 
+ * Deals with all information related to the the portfolio of financial assets 
+*/ 
+
+// Header file
+#include "Portfolio.h" 
+
+// Libraries 
+#include <iostream> 
+
+using namespace std; 
+
+// Methods 
---
+++ Portfolio.h
@@ -0,0 +1,23 @@
+/**
+ * Portfolio.h
+ * Header for the Portfolio class
+*/ 
+
+#ifndef _PORTFOLIO_H_
+#define _PORTFOLIO_H_
+
+// Libraries
+#include <iostream> 
+
+using namespace std; 
+
+class Portfolio {
+    public: 
+    // public methods go here 
+
+    private: 
+    //private methods go here 
+
+}; // class Portfolio
+
+#endif //_PORTFOLIO_H_
