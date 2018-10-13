56c7bfe56732cef32ea670623ffdede3411dd347
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-13 14:01:58 -0600

    Forecast structure

---
+++ Forecast.cpp
@@ -0,0 +1,14 @@
+/**
+ * Forecast.cpp 
+ * Deals with all information related to the the portfolio of financial assets 
+*/ 
+
+// Header file
+#include "Forecast.h" 
+
+// Libraries 
+#include <iostream> 
+
+using namespace std; 
+
+// Methods 
---
+++ Forecast.h
@@ -0,0 +1,23 @@
+/**
+ * Forecast.h
+ * Header for the Portfolio class
+*/ 
+
+#ifndef _FORECAST_H_
+#define _FORECAST_H_
+
+// Libraries
+#include <iostream> 
+
+using namespace std; 
+
+class Forecast {
+    public: 
+    // public methods go here 
+
+    private: 
+    //private methods go here 
+
+}; // class Forecast
+
+#endif //_FORECAST_H_
