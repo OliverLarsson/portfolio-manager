5eeca975c000ecb1bcd192bdaef86161825981b7
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-13 16:50:30 -0600

    Added commentary to main

--- main.cpp
+++ main.cpp
@@ -12,20 +12,22 @@
 
 // Libraries 
 #include <iostream> 
-#include <istream> 
 
 using namespace std; 
 
-int main() {
+int main() {   
 
+    string name; 
     int age; 
     int wealth;
     int risk; 
     Sector sector; 
     string sector_; 
-    
+
     // Get parameters for Investor class
     cout << "Welcome to your Portfolio Manager! Please provide some more information about yourself." << endl; 
+    cout << "What is your name? ";
+    getline(cin, name); 
     cout << "What is your age? "; 
     cin >> age; 
     cout << "How much money do you have for your portfolio? (Please exclude $ and commas) "; 
@@ -36,14 +38,18 @@ int main() {
     cin >> sector_; // cannot cin >> to an enum, so cin to string and assign to enum 
     if(sector_ == "t") {
         sector = Sector::tech;  
-    } else if(sector_ == "i") {
-        sector = Sector::industrial; 
     } else {
-        cout << "That is not a sector. Please enter technology (t) or industrial (i): ";
-        cin >> sector_;
-    }
+        sector = Sector::industrial; 
+    }   
 
-    Investor investor(age, wealth, risk, sector); 
+    Investor investor(name, age, wealth, risk, sector); 
+
+    cout << "From what you provided, these are your attributes:" << endl; 
+    cout << "   Name: " << investor.get_name() << "." << endl;
+    cout << "   Age: " << investor.get_age() << "." << endl;
+    cout << "   Wealth: " << investor.get_wealth() << "." << endl;
+    cout << "   Risk tolerance: " << investor.get_risk() << "." << endl;
+    cout << "   Sector preference: " << investor.get_sector() << "." << endl;
 
 
 
