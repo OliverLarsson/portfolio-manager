69c11d5839732e8030119db08ed817f0265d0c05
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-13 13:29:54 -0600

    main.cpp structure

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/**
+ * Oliver Larsson 
+ * CU Boulder - CSCI 3010 - Felix Muzny 
+ * Portfolio Manager Individual Project 
+*/ 
+
+// My header files 
+#include "Investor.h" 
+#include "Assets.h"
+#include "Portfolio.h"
+#include "Forecast.h" 
+
+// Libraries 
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+
+
+
+
+
+    
+    return 0; 
+}
