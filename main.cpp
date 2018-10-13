5e96dc63ffb52dbfe4cfaafaa2888b28a96c6ef1
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-13 16:10:08 -0600

    Initial main functionality for getting an investor's attributes and creating investor object

--- main.cpp
+++ main.cpp
@@ -12,10 +12,40 @@
 
 // Libraries 
 #include <iostream> 
+#include <istream> 
 
 using namespace std; 
 
 int main() {
+
+    int age; 
+    int wealth;
+    int risk; 
+    Sector sector; 
+    string sector_; 
+    
+    // Get parameters for Investor class
+    cout << "Welcome to your Portfolio Manager! Please provide some more information about yourself." << endl; 
+    cout << "What is your age? "; 
+    cin >> age; 
+    cout << "How much money do you have for your portfolio? (Please exclude $ and commas) "; 
+    cin >> wealth; 
+    cout << "On a whole number scale of 1 to 10, 1 being lowest, how tolerant to investment risk are you? "; 
+    cin >> risk; 
+    cout << "Would you prefer to invest in technology (t) or industrial (i) businesses? "; 
+    cin >> sector_; // cannot cin >> to an enum, so cin to string and assign to enum 
+    if(sector_ == "t") {
+        sector = Sector::tech;  
+    } else if(sector_ == "i") {
+        sector = Sector::industrial; 
+    } else {
+        cout << "That is not a sector. Please enter technology (t) or industrial (i): ";
+        cin >> sector_;
+    }
+
+    Investor investor(age, wealth, risk, sector); 
+
+
 
 
 
