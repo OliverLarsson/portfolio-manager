76a120f4f664aefeadcdfd1b03e6c6d051d2f7ea
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-17 16:42:44 -0600

    Changed to a Sqlite database

--- main.cpp
+++ main.cpp
@@ -18,19 +18,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-  Include directly the different
-  headers from cppconn/ and mysql_driver.h + mysql_util.h
-  (and mysql_connection.h). This will reduce your build time!
-
-#include "mysql_connection.h"
-
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-*/
-
 using namespace std;
 
 int main() {
