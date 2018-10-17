bea8c4b7a92d3bd57c10a78c46b6e49c4a75ce34
OliverLarsson <oliver.larsson@colorado.edu>
2018-10-17 13:20:50 -0600

    Includes for MySQL C++ Connection

--- Investor.cpp
+++ Investor.cpp
@@ -10,6 +10,18 @@
 #include <iostream> 
 #include <iterator>
 //#include <Ultralight/Ultralight.h> // include for Ultralight API 
+/*
+  Include directly the different
+  headers from cppconn/ and mysql_driver.h + mysql_util.h
+  (and mysql_connection.h). This will reduce your build time!
+
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+*/
 
 using namespace std; 
 
