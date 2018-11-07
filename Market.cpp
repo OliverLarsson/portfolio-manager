b98cb2236a599c5ae8f8a2d698f70b08e77cd8ef
OliverLarsson <oliver.larsson@colorado.edu>
2018-11-07 13:57:11 -0700

    Market updates

--- Market.cpp
+++ Market.cpp
@@ -11,7 +11,55 @@
 //#include <Ultralight/Ultralight.h> // include for Ultralight API 
 #include <sqlite3.h> // header file for the SQLite database
 
-
 using namespace std; 
 
 // Methods 
+
+/**
+ * Constructor
+ * Takes parameters for Market industry 
+*/
+Market::Market(string sector) {
+    sector_ = sector; 
+
+} 
+                                
+/**
+ * print_market
+ * Queries entire db market  
+*/
+void Market::print_market() {
+
+} 
+
+/**
+ * print_by_price
+ * Queries db market table by price  
+*/
+void Market::print_by_price() {
+
+} 
+
+/**
+ * print_by_ticker
+ * Queries db market table by ticker  
+*/
+void Market::print_by_ticker() {
+
+} 
+
+/**
+ * print_by_name
+ * Queries db market table by name  
+*/
+void Market::print_by_name() {
+
+} 
+
+/**
+ * print_by_variance
+ * Queries db market table by variance 
+*/
+void Market::print_by_variance() {
+
+} 
--- Market.h
+++ Market.h
@@ -16,16 +16,13 @@ using namespace std;
 // runs queries for stocks in decided sector
 class Market {
     public: 
-       Market(string sector); // creates a Market with only the sector (t or i / DJIA or NASDAQ) that the user asks for 
-                                // maybe create a private struct to equal the sqlite3 struct? 
-                                // need to access the 'sector' rows of Market database
-                                
+       Market(string sector); // constructor
 
-       void print_market(); // prints Technology if Market object created with t and Industrial if created with i 
-       void print_by_price(); // prints a specific query based on the price limit a user gives 
-       void print_by_ticker(); // prints the information of an asset by ticker 
-       void print_by_name(); // prints the information of an asset by name 
-       void print_by_variance(); // prints the information of an asset by YTD variance limit
+       void print_market();  
+       void print_by_price(); 
+       void print_by_ticker(); 
+       void print_by_name(); 
+       void print_by_variance(); 
 
     private: 
         string sector_; // needed for queries done by price, ticker, name, or variance
