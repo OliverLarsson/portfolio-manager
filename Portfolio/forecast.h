ef949b4310366de291afa39576767f3ecbb0f579
OliverLarsson <oliver.larsson@colorado.edu>
2018-12-12 18:43:23 -0700

    More commenting

--- Portfolio/forecast.h
+++ Portfolio/forecast.h
@@ -1,5 +1,23 @@
 /**
- * Attempted to make a Factory desing in QT but the Q_OBJECT that creates the UI is not copyable so that wasn't ever going to be made possible
+ * Same as the Portfolio Singleton implementation, I ran into problems with implementing
+ *  the Factory design pattern in this class.
+ *
+ * I tried creating the three forecasting method classes inherited from the Forecast.
+ *  The issue I ran into is the Q_OBJECT macro in the beginning of the Forecast class
+ *  isn't copyable since it's there to tell the compiler that there are GUI elements
+ *  in the class. What I think I understand from reading QT docs is that this cannot
+ *  be created multiple times since it's used in the compiling process.
+ *
+ * Another issue I ran into was with the QWidget *parent. As I explained in the
+ *  Portfolio class header file, the explicit declaration of the constructor is
+ *  designed (to the best of my understanding) not allow reassigning to the class
+ *  object. This design goes directly against the Factory design pattern since it
+ *  would require that the whole GUI create a new class object inherited from the
+ *  parent class.
+ *
+ * I thought about implementing some pseudo-Factory method where it runs certain
+ *  functions based on the forecast that should be run, but that just involves re-writing
+ *  methods with maybe a few variables that are different.
 */
 
 #ifndef FORECAST_H
@@ -40,7 +58,7 @@ private:
     double risk_profile_;
 
     int choice_;
-    int c_i_; // confidence interval
+    int c_i_;
 };
 
 #endif // FORECAST_H
--- Portfolio/portfolio.h
+++ Portfolio/portfolio.h
@@ -1,3 +1,20 @@
+/**
+ * I tried to implement the Singleton design pattern but I couldn't make it work.
+ *  Instead of keeping the code that I tried I'll try to explain what happened.
+ *
+ * The implementation that I used in the console app (portfolio-manager/Code/) wasn't suited
+ *  for this because I don't generally use to portfolio object in this QT version.
+ *  Instead I use the basic widgets and slots to draw and show information to the user.
+ *  Additionally, with how the application is set up in this UI, the user cannot create
+ *  multiple portfolio objects. This isn't an effect of a design pattern, but rather just the
+ *  structure of the GUI.
+ *
+ * Furthermore, the explicit constructor declaration ensures that no other QWidget objects
+ *  are implicitly converted to a Portfolio object. From what I understand in reading on QT is that
+ *  this replicates the Singleton design of not allowing objects to be reassigned.
+ */
+
+
 #ifndef PORTFOLIO_H
 #define PORTFOLIO_H
 
@@ -22,27 +39,12 @@ public:
     void add_contents();
     void set_units();
 
-    /**
-     * Attempt at Singleton implementation in QT -- works on console app but couldn't implement it correctly here
-     *
-    static Portfolio& GetPortfolio() {
-        static Portfolio portfolio;
-        return portfolio;
-    }
-
-    Portfolio(Portfolio const&) = delete;
-    void operator = (Portfolio const&) = delete;
-    */
-
 private slots:
     void on_pushButton_clicked();
 
     void on_pushButton_2_clicked();
 
 private:
-    // Attempt cont. with a private constructor
-    // Portfolio(string name, int age, double wealth, double risk_t, double risk_r, string sector, double risk_profile, QWidget *parent = nullptr);
-
     Ui::Portfolio *ui;
 
     string name_;
